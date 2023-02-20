e4514084830b7a712f106f26ca5f4fd2b7c2a529
82476 <82746@proton.me>
2023-02-20 02:58:58 +0200

    nicer printing + support for -r option

--- ui/cli_ui.py
+++ ui/cli_ui.py
@@ -24,31 +24,73 @@ def print_help_msg() -> None:
         print("")
         print("Options:")
         print("  -h --help      Show this screen.")
+        print("  -s --searches  Print saved searches and exit.")
+        print("  -p --pages     Print saved pages and exit.")
         print("  -r --raw       Don't format output.")
-        print("  -s --searches  Print saved searches")
-        print("  -p --pages     Print saved pages")
         print_supported_languages()
 
         return
 
-def print_saved_searches() -> int:
+
+
+def __group_by_dates(list:list[tuple]) -> dict:
+        """Groups tuples with text and datetime into a dictionary by date as tuples containing text and time. 
+        Util for print_saved_searches and print_saved_pages.
+        """
+        date_groups = {}
+        for l in list:
+                text = l[0]
+                datetime = l[1]
+                date = datetime.split(" ")[0]
+                time = datetime.split(" ")[1]
+
+                try:
+                        date_groups[date].append((text,time))
+                except:
+                        date_groups[date] = [(text,time)]
+
+        return date_groups
+
+def print_saved_searches(do_formatting=True) -> int:
         db = Database()
         searches = db.get_saved_searches()
-        if searches:
-                for s in searches:
-                        print(" | ".join(list(s)))
-
-                return 0
-        else:
+        if not searches:
+                print("No saved searches.")
                 return 1
 
-def print_saved_pages() -> int:
+        if do_formatting:
+                groups = __group_by_dates(searches)
+
+                for date, text_list in groups.items():
+                        print("\033[1;31m" + date + "\033[m")
+                        for t in text_list:
+                                output_str = "\033[35;2m" + "▏   " + t[1] + "\033[22;3m" + " " + f"\"{t[0]}\"" + "\033[0m" + "\033[0m"
+                                print(output_str)
+        else:
+                for s in searches:
+                        print("|".join(list(s)))
+
+        return 0
+        
+
+def print_saved_pages(do_formatting=True) -> int:
         db = Database()
         pages = db.get_saved_pages()
-        if pages:
-                for p in pages:
-                        print(" | ".join(list(p)))
-
-                return 0
-        else:
+        if not pages:
+                print("No saved searches.")
                 return 1
+
+
+        if do_formatting:
+                groups = __group_by_dates(pages)
+
+                for date, text_list in groups.items():
+                        print("\033[1;31m" + date + "\033[m")
+                        for t in text_list:
+                                output_str = "\033[35;2m" + "▏   " + t[1] + "\033[22m" + " " + f"{t[0]}" + "\033[0m" + "\033[0m"
+                                print(output_str)
+        else:
+                for p in pages:
+                        print("|".join(list(p)))
+
+        return 0
