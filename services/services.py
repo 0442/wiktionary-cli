773b3a185c6e917bc6b87731d2a5859024540734
82476 <82746@proton.me>
2023-02-20 21:15:47 +0200

    refac

--- services/services.py
+++ services/services.py
@@ -1,4 +1,4 @@
-from tools.wikiparser import WikiParser
+from tools.wikiparser import WikiParser, Section
 from services.wiki_api import WikiApi
 import ui.cli_ui as cli_ui
 import tools.languages as languages
@@ -6,149 +6,138 @@ import tools.parsing_utils as parsing
 import tools.config as config
 from services.db import Database
 
-# TODO: split functions into smaller chunks.
-
-def get_wiki_page(args:list[str], do_formatting=True, force_web=False) -> int:
-        if len(args) < 2:
-                cli_ui.print_help_msg()
-                exit(1)
-        
-        lang = args[0]
-        title = args[1]
-        sect_path = None
-        if len(args) >= 3:
-                sect_path = args[2]
-        
-        # check if user gave a usable language
-        if lang not in languages.supported:
-                print(f"Unsupported language: \"{lang}\"")
-                #print_languages.supported()
-                return 1
-
-
-        wiki = WikiApi(lang, "wikipedia")
-        page_info = wiki.get_page(title)
+def __get_page_from_wiki(page_name:str, lang:str, site:str) -> Section:
+        wiki = WikiApi(lang, site)
+        page_info = wiki.get_page(page_name)
 
         if not page_info[0]:
-                cli_ui.word_not_found(title, lang)
+                cli_ui.word_not_found(page_name, lang)
                 return 1
         
         page_title = page_info[0]
         page_id = page_info[1]
         page_text = page_info[2]
 
-        page = WikiParser(page_text,page_title)
+        page = WikiParser(page_text, page_title)
 
-        root_sect = page.page_root_section
+        return page
 
-        if sect_path:
-                sect = root_sect.find(sect_path)
-                if sect:
-                        if do_formatting:
-                                print(parsing.format_section_content(sect, lang))
-                        else:
-                                print(sect.content)
-                else:
-                        print(f"No section '{args[2]}'.")
-                        return 0
-        # if no section given, print root_section (__str__(), page's structure)
-        else:
-                if do_formatting:
-                        print(root_sect)
-                else:
-                        print(page_text)
-
+def __search_wiki(search:str, lang:str, site:str) -> int:
+        wiki = WikiApi(lang, site)
+        results = wiki.search(search)
+        if not results:
+                return 1
+        [print(r) for r in results]
         return 0
 
+def __get_matching_sections(page:WikiParser, target_path:str, lang:str) -> list[Section]:
+        root_section = page.page_root_section
 
-def get_dictionary_entry(args:list[str], do_formatting=True, force_web=False, do_search=False) -> int:
+        matching_sections = []
 
+        if not target_path:
+                return []
+
+        # if a section is given, print that section (or a group of sections, if target_path is a key word associated with some arbitrary group of sections, e.g. 'definitions', which matches Nouns, Verbs, etc..)
+        if target_path.lower() == "definitions" or target_path.lower() == "defs":
+                for wc in languages.definitions[lang]:
+                        target_sect = root_section.find(languages.abbrev_table[lang][lang] + "/" + wc)
+                        if target_sect:
+                                matching_sections.append(target_sect)
+                
+
+        else:
+                target_sect = root_section.find(target_path)
+                if target_sect:
+                        matching_sections.append(target_sect)
+
+
+        if len(matching_sections) == 0:
+                print(f"No sections found with \"{target_path}\"")
+                return None
+        else:
+                return matching_sections
+
+
+
+def dictionary(args:list[str], do_formatting=True, force_web=False, do_search=False) -> int:
         if len(args) < 2:
                 cli_ui.print_help_msg()
                 exit(1)
         
         lang = args[0]
         word = args[1]
-        sect_path = None
+        target_path = None
         if len(args) >= 3:
-                sect_path = args[2]
+                target_path = args[2]
+        else:
+                target_path = "/"
 
-        # check if user gave a usable language
         if lang not in languages.supported:
                 print(f"Unsupported language: \"{lang}\"\nSee the help message for supported languages.")
-                #print_languages.supported()
                 return 1
-
-
-                
 
         db = Database()
         db.save_search(word)
+
         if do_search:
-                wiki = WikiApi(lang, "wiktionary")
-                results = wiki.search(word)
-                if not results:
-                        return 1
-                [print(r) for r in results]
-                return 0
+                return __search_wiki(word, lang, "wiktionary")
 
         # try to find the page from local db, else get it from wiki
         page = db.load_page(word)
 
         if not page or force_web:
-                wiki = WikiApi(lang, "wiktionary")
-                page_info = wiki.get_page(word)
-
-                if not page_info[0]:
-                        cli_ui.word_not_found(word, lang)
-                        return 1
-                
-                page_title = page_info[0]
-                page_id = page_info[1]
-                page_text = page_info[2]
-
-                page = WikiParser(page_text,page_title)
-                root_sect = page.page_root_section
+                page = __get_page_from_wiki(word, lang, "wiktionary")
                 db.save_page(page)
+
+        matching_sects = __get_matching_sections(page, target_path, lang)
+        if not matching_sects:
+                return 1
+
+        if target_path.endswith("/"):
+                print_tree = True
         else:
-                root_sect = page.page_root_section
+                print_tree = False
+
+        return cli_ui.print_sections(matching_sects, lang, print_tree=print_tree, do_formatting=do_formatting)
 
 
-        # if a section is given, print that section (or a group of sections, if sect_path is a key word associated with some arbitrary group of sections, e.g. 'definitions', which matches Nouns, Verbs, etc..)
-        if sect_path:
-                if sect_path.lower() == "definitions" or sect_path.lower() == "defs":
-                        for wc in languages.definitions[lang]:
-                                sect = root_sect.find(languages.abbrev_table[lang][lang]+ "/" + wc)
-                                if sect:
-                                        if do_formatting:
-                                                sect_str = parsing.format_section_content(sect, lang)
-                                        else:
-                                                sect_str = sect.content
+def wikipedia(args:list[str], do_formatting=True, force_web=False, do_search=False) -> int:
+        # TODO implement database
 
-                                        print( sect_str+'\n\n' if sect_str is not None else "None", end="")
+        if len(args) < 2:
+                cli_ui.print_help_msg()
+                exit(1)
+        
+        lang = args[0]
+        title = args[1]
+        target_path = None
+        if len(args) >= 3:
+                target_path = args[2]
+        
+        if lang not in languages.supported:
+                print(f"Unsupported language: \"{lang}\"")
+                return 1
 
-                else:
-                        sect = root_sect.find(sect_path)
-                        if sect:
-                                if do_formatting:
-                                        print(parsing.format_section_content(sect, lang))
-                                else:
-                                        print(sect.content)
-                        else:
-                                print(f"No section '{args[2]}'.")
-                                return 1
+        if do_search:
+                return __search_wiki(title, lang, "wikipedia")
+        
+        page = __get_page_from_wiki(title, lang, "wiktionary")
 
-        # if no section given, print whole page(either root_sect.__str__() or the raw text, if command was run with option '-r' )
+        matching_sects = __get_matching_sections(page, target_path, lang)
+        if not matching_sects:
+                return 1
+
+        if target_path.endswith("/"):
+                print_tree = True
         else:
-                if do_formatting:
-                        print(root_sect)
-                else:
-                        print(page.page_text)
+                print_tree = False
 
-        return 0
+        return cli_ui.print_sections(matching_sects, lang, print_tree=print_tree, do_formatting=do_formatting)
 
 
-def translate_word(args:list[str]) -> int:
+
+def translation(args:list[str]) -> int:
         if len(args) != 3:
                 cli_ui.print_help_msg()
                 exit(1)
@@ -160,7 +149,7 @@ def translate_word(args:list[str]) -> int:
         if from_lang == to_lang:
                 print("Give two different languages for translation.")
                 return 1
-        # check if user gave a usable language
+
         if from_lang not in languages.supported or to_lang not in languages.supported:
                 print(f"Unsupported language:",end="")
                 if from_lang not in languages.supported:
@@ -169,48 +158,4 @@ def translate_word(args:list[str]) -> int:
                         print(f" \"{to_lang}\"",end="")
                 print()
 
-                #print_languages.supported()
                 return 1
-
-
-
-        wiki = WikiApi(from_lang, "wikipedia")
-        page_info = wiki.get_page(word)
-
-        if not page_info[0]:
-                cli_ui.word_not_found(word, from_lang)
-                return 1
-        
-        page_title = page_info[0]
-        page_id = page_info[1]
-        page_text = page_info[2]
-        
-        parser = WikiParser(page_text, word)
-        page = parser.page_root_section
-
-        tr_str = ""
-        if from_lang == "fi":
-                for wc in languages.definitions[from_lang]:
-                        tr_section = parser.find(f"{word}/{languages.abbrev_table[from_lang][from_lang].capitalize()}/{wc}/Käännökset", from_lang).content
-                        if not tr_section:
-                                continue
-                        tr_str += wc + '\n\n'
-                        tr_str += "\n".join([line.removeprefix(f"*{languages.abbrev_table[from_lang][to_lang]}: ") for line in tr_section.splitlines() if line.startswith(f"*{languages.abbrev_table[from_lang][to_lang]}")])
-
-        elif from_lang == "en":
-                full_str = ""
-                for wc in languages.definitions[from_lang]:
-                        path=f"{word}/{languages.abbrev_table[from_lang][from_lang]}/{wc}/Translations"
-                        tr_section = page.find(path)
-                        if not tr_section:
-                                continue
-                        full_str += wc + '\n\n'
-                        full_str += parser.parse_translations_section(parser.format_section_content(path,to_lang),from_lang,to_lang)
-                tr_str = full_str
-
-        if tr_str:
-                print(tr_str)
-        else:
-                print(f"No translation from {languages.abbrev_table['en'][from_lang]} to {languages.abbrev_table['en'][to_lang]} found for \"{word}\".")
-
-        return 0
--- ui/cli_ui.py
+++ ui/cli_ui.py
@@ -1,5 +1,7 @@
 import tools.languages as languages
 from services.db import Database
+from tools.wikiparser import *
+import tools.parsing_utils as parsing
 
 # Functions for printing some output
 def word_not_found(word:str, search_lang:str) -> None:
@@ -34,22 +36,56 @@ def print_help_msg() -> None:
 
 def __group_by_dates(list:list[tuple]) -> dict:
         """Groups tuples with text and datetime into a dictionary by date as tuples containing text and time. 
+
         Util for nicer printing for print_saved_searches and print_saved_pages functions.
         """
         date_groups = {}
         for l in list:
-                #id = l[0]
-                text = l[1] 
-                datetime = l[2] 
+                id = str(l[0])
+                text = str(l[1])
+                datetime = str(l[2])
+                try:
+                        count = str(l[3])
+                except:
+                        count = None
+
                 date = datetime.split(" ")[0]
                 time = datetime.split(" ")[1]
 
                 try:
-                        date_groups[date].append((text,time))
+                        date_groups[date].append((id,text,time,count))
                 except:
-                        date_groups[date] = [(text,time)]
+                        date_groups[date] = [(id,text,time,count)]
 
         return date_groups
+
+def __group_consecutive_searches(searches:list[tuple]):
+        """Groups similar consecutive searches and counts them for more compact output.
+        """
+        searches = searches.copy()
+        grouped_searches = []
+
+        prev_search = ("","")
+        search_count = 1
+        first_search_datetime = None
+
+        while len(searches) > 0:
+                cur_search = searches.pop(0)
+
+                if cur_search[1] == prev_search[1]:
+                        search_count += 1
+                        prev_search = cur_search
+                        continue
+                else:
+                        first_search_datetime = cur_search[2]
+                        prev_search = cur_search
+
+                grouped_searches.append((cur_search[0], cur_search[1], first_search_datetime, search_count))
+                search_count = 1
+
+        return grouped_searches
+
+
 
 def print_saved_searches(do_formatting=True) -> int:
         """Print searches that are saved into the database
@@ -62,12 +98,17 @@ def print_saved_searches(do_formatting=True) -> int:
                 return 1
 
         if do_formatting:
-                groups = __group_by_dates(searches)
+                search_groups = __group_consecutive_searches(searches)
+                date_groups = __group_by_dates(search_groups)
 
-                for date, searches in groups.items():
-                        print("\033[1;31m" + date + "\033[m")
+                for date, searches in date_groups.items():
+                        print("\033[1;31m" + date + "\033[0m")
                         for s in searches:
-                                output_str = "\033[35;2m" + "▏   " + s[1] + "\033[22;3m" + " " + f"\"{s[0]}\"" + "\033[0m" + "\033[0m"
+                                id, text, time, count = s[0], s[1], ":".join(s[2].split(":")[:2]), s[3]
+                                if count == "1":
+                                        output_str = "\033[35;2m" + time + "\033[22m" + " " + f"{text}" + "\033[0m"
+                                else:
+                                        output_str = "\033[35;2m" + time + "\033[22m" + " " + f"{text}" + f"({count})" + "\033[0m"
                                 print(output_str)
         else:
                 for s in searches:
@@ -93,7 +134,8 @@ def print_saved_pages(do_formatting=True) -> int:
                 for date, page_names in groups.items():
                         print("\033[1;31m" + date + "\033[m")
                         for p in page_names:
-                                output_str = "\033[35;2m" + "▏   " + p[1] + "\033[22m" + " " + f"{p[0]}" + "\033[0m" + "\033[0m"
+                                id,name,datetime = p[0], p[1], ":".join(p[2].split(":")[:2])
+                                output_str = "\033[35;2m" + datetime + "\033[22m" + " " + f"{name}" + "\033[0m"
                                 print(output_str)
         else:
                 for p in pages:
@@ -101,3 +143,23 @@ def print_saved_pages(do_formatting=True) -> int:
                         print("|".join(values))
 
         return 0
+
+
+
+def print_sections(sections:list[Section], lang, print_tree=False, do_formatting=True):
+        if print_tree:
+                for sect in sections:
+                        print(sect)
+                
+                return 0
+
+
+        for sect in sections:
+                if do_formatting:
+                        sect_str = parsing.format_section_content(sect, lang)
+                else:
+                        sect_str = sect.content
+
+                print( sect_str+'\n\n' if sect_str is not None else "None", end="")
+
+                return 0
